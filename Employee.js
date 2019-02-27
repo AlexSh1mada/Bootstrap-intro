68bc9e997a6d83c0ee826d8dd88b03aa1178a2a9
Alex <alexbuerom@gmail.com>
2019-02-26 19:36:25 -0600

    third commit

---
+++ Employee.js
@@ -0,0 +1,15 @@
+export default class Employee
+{
+    constructor(name, email, birthday)
+    {
+        this._name = name.toUpperCase();
+        this._email = email;
+        this._birthday = birthday;
+    }
+
+    //returns employee's age
+    getAge()
+    {
+        
+    }
+}
